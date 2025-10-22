bcfc7fa9e49a6476be97ed5069b5ff4c3a89b9a9
Ghazanfar Mehmood <ghazanfar4154@gmail.com>
2025-10-22 16:29:36 +0500

    Updated

--- lib/training/presentation/widgets/advanced_tab_data.dart
+++ lib/training/presentation/widgets/advanced_tab_data.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../armory/presentation/bloc/armory_bloc.dart';
-import '../../../armory/presentation/bloc/armory_event.dart';
 import '../../../armory/presentation/bloc/armory_state.dart';
 import '../../../armory/domain/entities/armory_loadout.dart';
 import '../../data/model/programs_model.dart';
